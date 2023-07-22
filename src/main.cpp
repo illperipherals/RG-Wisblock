902c154bb7bb737d6510fb841676d1006fa9c633
tteague <travis.teague@gmail.com>
2023-07-22 16:37:19 -0500

    added RGB LED

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,8 @@
 #include <Arduino.h>
 #include <LoRaWan-RAK4630.h> //http://librarymanager/All#SX126x
 #include <SPI.h>
+#include <Wire.h>
+#include <NCP5623.h>
 
 // RAK4630 supply two LEDs
 #ifndef LED_BUILTIN
@@ -36,14 +38,16 @@
 #define LED_BUILTIN2 36
 #endif
 
+NCP5623 rgb;
+
 bool doOTAA = true;                                               // OTAA is used by default.
 #define SCHED_MAX_EVENT_DATA_SIZE APP_TIMER_SCHED_EVENT_DATA_SIZE /**< Maximum size of scheduler events. */
 #define SCHED_QUEUE_SIZE 60                                       /**< Maximum number of events in the scheduler queue. */
-#define LORAWAN_DATERATE DR_0                                     /*LoRaMac datarates definition, from DR_0 to DR_5*/
+#define LORAWAN_DATERATE DR_1                                     /*LoRaMac datarates definition, from DR_0 to DR_5*/
 #define LORAWAN_TX_POWER TX_POWER_5                               /*LoRaMac tx power definition, from TX_POWER_0 to TX_POWER_15*/
-#define JOINREQ_NBTRIALS 3                                        /**< Number of trials for the join request. */
+#define JOINREQ_NBTRIALS 5                                        /**< Number of trials for the join request. */
 DeviceClass_t g_CurrentClass = CLASS_C;                           /* class definition*/
-LoRaMacRegion_t g_CurrentRegion = LORAMAC_REGION_US915;           /* Region:EU868*/
+LoRaMacRegion_t g_CurrentRegion = LORAMAC_REGION_US915;           /* Region:US915*/
 lmh_confirm g_CurrentConfirm = LMH_UNCONFIRMED_MSG;               /* confirm/unconfirm packet definition*/
 uint8_t gAppPort = LORAWAN_APP_PORT;                              /* data port*/
 
@@ -55,8 +59,7 @@ static lmh_param_t g_lora_param_init = {
     LORAWAN_PUBLIC_NETWORK,
     JOINREQ_NBTRIALS,
     LORAWAN_TX_POWER,
-    LORAWAN_DUTYCYCLE_OFF
-};
+    LORAWAN_DUTYCYCLE_OFF};
 
 // Foward declaration
 static void lorawan_has_joined_handler(void);
@@ -74,13 +77,12 @@ static lmh_callback_t g_lora_callbacks = {
     lorawan_rx_handler,
     lorawan_has_joined_handler,
     lorawan_confirm_class_handler,
-    lorawan_join_failed_handler
-};
+    lorawan_join_failed_handler};
 
 // OTAA keys !!!! KEYS ARE MSB !!!!
-uint8_t nodeDeviceEUI[8] = {0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x33, 0x33};
-uint8_t nodeAppEUI[8] = {0xB8, 0x27, 0xEB, 0xFF, 0xFE, 0x39, 0x00, 0x00};
-uint8_t nodeAppKey[16] = {0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88, 0x88};
+uint8_t nodeDeviceEUI[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+uint8_t nodeAppEUI[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+uint8_t nodeAppKey[16] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 // ABP keys
 uint32_t nodeDevAddr = 0x260116F8;
@@ -100,6 +102,10 @@ static uint32_t count_fail = 0;
 
 void setup()
 {
+  // enable RAK14001
+  pinMode(WB_IO6, OUTPUT);
+  digitalWrite(WB_IO6, HIGH);
+
   pinMode(LED_GREEN, OUTPUT);
   digitalWrite(LED_GREEN, LOW);
   pinMode(LED_BLUE, OUTPUT);
@@ -122,6 +128,10 @@ void setup()
       break;
     }
   }
+
+  // If using Native I2C
+  Wire.begin();
+  Wire.setClock(100000);
 
   // Initialize LoRa chip.
   lora_rak4630_init();
@@ -181,6 +191,17 @@ void setup()
   }
   Serial.println("=====================================");
 
+  if (!rgb.begin())
+  {
+    Serial.println("RAK14001 not found on the I2C line");
+    while (1)
+      ;
+  }
+  else
+  {
+    Serial.println("RAK14001 Found. Beginning execution");
+  }
+
   // create a user timer to send data to server period
   uint32_t err_code;
   err_code = timers_init();
@@ -210,8 +231,7 @@ void setup()
       g_lora_param_init,
       doOTAA,
       g_CurrentClass,
-      g_CurrentRegion
-  );
+      g_CurrentRegion);
 
   if (err_code != 0)
   {
@@ -223,10 +243,81 @@ void setup()
   lmh_join();
 }
 
+/**
+ * LED light up Gradually
+ *
+ * @param red          0-255
+ * @param green        0-255
+ * @param blue         0-255
+ * @param stepNum      0-31
+ * @param msPerStep    1ms-248ms
+ */
+void gradualLightUp(uint8_t red, uint8_t green, uint8_t blue, uint8_t stepNum, uint8_t msPerStep)
+{
+  rgb.setColor(red, green, blue);
+
+  // set dimming up end, range is 1 to 30
+  rgb.setGradualDimmingUpEnd(stepNum);
+
+  // set dimming step ms, range is 1 ms to 248 ms
+  rgb.setGradualDimming(msPerStep);
+
+  delay(stepNum * msPerStep);
+}
+
+/**
+ * LED dark down Gradually
+ *
+ * @param red          0-255
+ * @param green        0-255
+ * @param blue         0-255
+ * @param stepNum      0-31
+ * @param msPerStep    1ms-248ms
+ */
+void gradualDarkDown(uint8_t red, uint8_t green, uint8_t blue, uint8_t stepNum, uint8_t msPerStep)
+{
+  rgb.setColor(red, green, blue);
+
+  // set dimming up end, range is 1 to 30
+  rgb.setGradualDimmingDownEnd(31 - stepNum);
+
+  // set dimming step ms, range is 1 ms to 248 ms
+  rgb.setGradualDimming(msPerStep);
+
+  delay(stepNum * msPerStep);
+}
+
 void loop()
 {
-  // Put your application tasks here, like reading of sensors,
-  // Controlling actuators and/or other functions.
+  // // Put your application tasks here, like reading of sensors,
+  // // Controlling actuators and/or other functions.
+  // // RED
+  // gradualLightUp(100, 0, 0, 30, 100);
+  // gradualDarkDown(100, 0, 0, 30, 100);
+
+  // // GREEN
+  // gradualLightUp(0, 100, 0, 30, 100);
+  // gradualDarkDown(0, 100, 0, 30, 100);
+
+  // // BLUE
+  // gradualLightUp(0, 0, 100, 30, 100);
+  // gradualDarkDown(0, 0, 100, 30, 100);
+
+  // // YELLOW
+  // gradualLightUp(100, 100, 0, 30, 100);
+  // gradualDarkDown(100, 100, 0, 30, 100);
+
+  // // PURPLE
+  // gradualLightUp(100, 0, 255, 30, 100);
+  // gradualDarkDown(100, 0, 255, 30, 100);
+
+  // // CYAN
+  // gradualLightUp(0, 100, 100, 30, 100);
+  // gradualDarkDown(0, 100, 100, 30, 100);
+
+  // // WHITE
+  // gradualLightUp(100, 100, 100, 30, 100);
+  // gradualDarkDown(100, 100, 100, 30, 100);
 }
 
 /**@brief LoRa function for handling HasJoined event.
@@ -264,11 +355,22 @@ static void lorawan_join_failed_handler(void)
  *
  * @param[in] app_data  Pointer to rx data
  *
- *  Command	  Base64	  Function
- *  G0	      RzA=	    Switch green LED off
- *  G1	      RzE=	    Switch green LED on
- *  B0	      QjA=	    Switch blue LED off
- *  B1	      QjE=	    Switch blue LED on
+ *  Command	  Base64	  HEX     Function
+ *  G0	      RzA=	    4730    Switch green LED off
+ *  G1	      RzE=	    4731    Switch green LED on
+ *  B0	      QjA=	    4230    Switch blue LED off
+ *  B1	      QjE=	    4231    Switch blue LED on
+ *  R0	      UjA=	    5230    Switch red LED off
+ *  R1	      UjE=	    5231    Switch red LED on
+ *
+ *  SC        U0M=      5343    Set RGB LED Color
+ *
+ * e.g.: set RGB LED (HEX):
+ * 5343 RED GREEN BLUE
+ * RED:   5343FF0000
+ * GREEN: 534300FF00
+ * BLUE:  53430000FF
+ *
  */
 void lorawan_rx_handler(lmh_app_data_t *app_data)
 {
@@ -278,6 +380,7 @@ void lorawan_rx_handler(lmh_app_data_t *app_data)
   for (int idx = 0; idx < app_data->buffsize; idx++)
   {
     Serial.printf("0x%0X", app_data->buffer[idx]);
+    Serial.println();
   }
   Serial.println();
 
@@ -291,7 +394,7 @@ void lorawan_rx_handler(lmh_app_data_t *app_data)
     }
     else
     {
-      // Switch LED off
+      // Switch LED on
       digitalWrite(LED_GREEN, HIGH);
     }
   }
@@ -305,8 +408,21 @@ void lorawan_rx_handler(lmh_app_data_t *app_data)
     }
     else
     {
-      // Switch LED off
+      // Switch LED on
       digitalWrite(LED_BLUE, HIGH);
+    }
+  }
+  if (app_data->buffer[0] == 'S')
+  {
+    if (app_data->buffer[1] == 'C')
+    {
+      // Serial.println("Set RGB LED Color");
+      rgb.setColor(app_data->buffer[2], app_data->buffer[3], app_data->buffer[4]);
+      rgb.setGradualDimmingUpEnd(30);
+      rgb.setGradualDimming(100);
+      rgb.setGradualDimmingDownEnd(1);
+      rgb.setGradualDimming(100);
+      delay(30 * 100);
     }
   }
 }
@@ -331,12 +447,12 @@ void send_lora_frame(void)
   uint32_t i = 0;
   memset(m_lora_app_data.buffer, 0, LORAWAN_APP_DATA_BUFF_SIZE);
   m_lora_app_data.port = gAppPort;
-  m_lora_app_data.buffer[i++] = 'H';
-  m_lora_app_data.buffer[i++] = 'e';
-  m_lora_app_data.buffer[i++] = 'l';
-  m_lora_app_data.buffer[i++] = 'l';
-  m_lora_app_data.buffer[i++] = 'o';
-  m_lora_app_data.buffer[i++] = '!';
+  // m_lora_app_data.buffer[i++] = 'H';
+  // m_lora_app_data.buffer[i++] = 'e';
+  // m_lora_app_data.buffer[i++] = 'l';
+  // m_lora_app_data.buffer[i++] = 'i';
+  // m_lora_app_data.buffer[i++] = 'u';
+  // m_lora_app_data.buffer[i++] = 'm';
   m_lora_app_data.buffsize = i;
 
   lmh_error_status error = lmh_send(&m_lora_app_data, g_CurrentConfirm);
@@ -358,7 +474,7 @@ void tx_lora_periodic_handler(void)
 {
   TimerSetValue(&appTimer, LORAWAN_APP_INTERVAL);
   TimerStart(&appTimer);
-  Serial.println("Sending frame now...");
+  // Serial.println("Sending frame now...");
   send_lora_frame();
 }
 
